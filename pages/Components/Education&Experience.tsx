075526c92337f0514787fece2187afe31b69c907
Abubakar <151372655+abubkar-afzal@users.noreply.github.com>
2025-11-23 18:46:53 +0500

    creating education and experience page basic structure

---
+++ pages/Components/Education&Experience.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+const Education_Experience = ()=>{
+    return<>
+    <div>
+        <div>
+            <div>
+                <div>Education</div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>
+                <div>
+                    <div>BSCS</div>
+                    <div>Virtual Universty </div>
+                    <div>Currently I am studing there.</div>
+                </div>
+
+            </div>
+
+            <div>
+                <div></div>
+                <div>
+                    <div>ICS Statistics</div>
+                    <div>Goverment Islamia Graduate College Railway Road Lahore.</div>
+                    <div>I got 912 marks in lahore board out of 1200.</div>
+                </div>
+
+            </div>
+            <div>
+                <div></div>
+                <div>
+                    <div>Matric in Biology</div>
+                    <div>Goverment Watan islamia High School Brandther Road Lahore.</div>
+                    <div>I got 855 marks in Lahore Borad out of 1100.</div>
+                </div>
+
+            </div>
+        </div>
+        <div>
+            <div>
+                <div>Experience</div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>
+                <div>
+                    <div>Full Stack Developer</div>
+                    <div>Virtual Universty </div>
+                    <div>Currently I am studing there.</div>
+                </div>
+
+            </div>
+
+            <div>
+                <div></div>
+                <div>
+                    <div>Manager</div>
+                    <div>Goverment Islamia Graduate College Railway Road Lahore.</div>
+                    <div>I got 912 marks in lahore board out of 1200.</div>
+                </div>
+
+            </div>
+            <div>
+                <div></div>
+                <div>
+                    <div>Computer Hardware</div>
+                    <div>Goverment Watan islamia High School Brandther Road Lahore.</div>
+                    <div>I got 855 marks in Lahore Borad out of 1100.</div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    </>
+}
+
+export default Education_Experience;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import FirstPage from "./Components/FirstPage";
 import About from "./Components/About";
+import Education_Experience from "./Components/Education&Experience";
 export default function Home() {
   return (
     <>
     <Navbar/>
     <FirstPage/>
     <About/>
+    <Education_Experience/>
     </>
   );
 }
