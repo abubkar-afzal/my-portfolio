81641b1dc9a0776b5cb333c6a6c32da80cfbd879
Abubakar <151372655+abubkar-afzal@users.noreply.github.com>
2025-12-28 15:10:19 +0500

    designing intro and navbar page
    There I just design the pages functionality and other things I will add soon.

--- pages/Components/FirstPage.tsx
+++ pages/Components/FirstPage.tsx
@@ -1,21 +1,42 @@
 import Image from "next/image";
 import React from "react";
 import img1 from "../../public/img1.jpg"
-
-const FirstPage = ()=>{
-    return<>
-        <div>
-            <div>
-                <div>Round text</div>
-                <div><Image src={img1} alt="Image" width={1020} height={1020}/></div>
-            </div>
-            <div>
-                <div>Hafiz Abubkar Afzal</div>
-                <div>Full Stack Developer</div>
-                <div>I apperciate your trust gratly.</div>
+import { motion } from 'framer-motion';
+const FirstPage = () => {
+    return <>
+        <div className="pt-[2rem] overflow-hidden">
+            <div className=" z-10">
+                <div className={`text-(--white) absolute right-4 top-5`}><div className="relative flex items-center justify-center w-16 h-16">
+                    <motion.svg
+                        animate={{ rotate: 360 }}
+                        transition={{ duration: 10, repeat: Infinity, ease: "linear" }}
+                        viewBox="0 0 100 100"
+                        className="w-full h-full"
+                    >
+                        <defs>
+                            <path
+                                id="circlePath"
+                                d="M 50, 50 m -40, 0 a 40,40 0 1,1 80,0 a 40,40 0 1,1 -80,0"
+                            />
+                        </defs>
+                        <text className="fill-black dark:fill-white font-mono text-3rem uppercase tracking-[0.2em]">
+                            <textPath xlinkHref="#circlePath">
+                                Full Stack Developer  
+                            </textPath>
+                        </text>
+                    </motion.svg>
+                </div></div>
                 <div>
-                    <button>View Work</button>
-                    <button>Contact Me</button>
+                    <Image src={img1} alt="Image" width={1020} height={1020} className="p-8"/>
+                </div>
+            </div>
+            <div className={`text-(--white) text-center`}>
+                <div className="text-[1.6rem] font-black">Hafiz Abubkar Afzal</div>
+                <div className="text-[1rem] font-thin">Full Stack Developer</div>
+                <div className="text-[1.1rem] font-arial my-2">I apperciate your trust greatly.</div>
+                <div className="flex flex-col space-y-2 items-center text-(--black)">
+                    <button className="bg-(--yellow) p-2 rounded-[1rem] w-[10rem]">View Work</button>
+                    <button className="bg-(--green) p-2 rounded-[1rem] w-[10rem]">Contact Me</button>
                 </div>
             </div>
         </div>
--- pages/Components/Navbar.tsx
+++ pages/Components/Navbar.tsx
@@ -1,60 +1,67 @@
 import React, { useState } from "react"
+import { MdOutlineMenu } from "react-icons/md";
+import { RxCross2 } from "react-icons/rx";
+import img1 from "../../public/img1.jpg"
+import Image from "next/image";
+import { FaFacebook, FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa";
+import { FaAddressCard, FaCamera, FaDownload, FaFileContract, FaHouse, FaSalesforce, FaServicestack, FaUser, FaVrCardboard } from "react-icons/fa6";
+import { DiVim } from "react-icons/di";
 const Navbar = () => {
     const [show, setShow] = useState(false);
     return <>
-    <div className="fixed z-20 text-red-800" onClick={()=>{setShow(!show)}}>
-                Icon
-            </div>
-        <div className={`bg-(--gray) text-(--white) w-[70vw] h-screen pt-[1rem] fixed ${show ? "left-0":"left-[-100vw]"} duration-[1s]`}>
-            
-            <div className="flex flex-col my-2 space-y-[2rem]">
-                <div>
-                <div>Image</div>
-                <div>Name</div>
-                <div>Developer</div>
-                <div className="flex space-x-2">
-                    <div>social</div>
-                    <div>social</div>
-                    <div>social</div>
-                    <div>socail</div>
+        <div className={`px-2 w-[70vw] h-10 fixed z-50 text-(--white) border-(--black)`} onClick={() => { setShow(!show) }}>
+            {show ? <RxCross2 className={`w-6 h-6`} /> : <MdOutlineMenu className="w-6 h-6" />}
+        </div>
+        <div className={`bg-(--gray) text-(--white) w-[70vw] h-screen pt-[1rem] fixed ${show ? "left-0" : "left-[-100vw]"} duration-[1s]`}>
+
+            <div className="flex flex-col my-2 space-y-[2rem] items-center text-center text-wrap">
+                <div className="flex flex-col items-center">
+                    <div><Image src={img1} alt="image" width={150} height={150} /></div>
+                    <div className="font-black text-[1.5rem]">Hafiz Abubakar Afzal</div>
+                    <div className="font-thin">Developer</div>
+                    <div className="flex space-x-8 m-4 text-[1.2rem]">
+                        <div className="cursor-pointer hover:text-(--mahroon)"><FaLinkedin /></div>
+                        <div className="cursor-pointer hover:text-(--mahroon)"><FaInstagram /></div>
+                        <div className="cursor-pointer hover:text-(--mahroon)"><FaFacebook /></div>
+                        <div className="cursor-pointer hover:text-(--mahroon)"><FaWhatsapp /></div>
+                    </div>
                 </div>
-                </div>
-                <div>
-                    <div className="flex space-x-2">
-                        <div>Icon</div>
+                <div className="items-left text-left w-[80%] space-y-2">
+                    <div className="flex space-x-2 items-center">
+                        <div><FaHouse /></div>
                         <div>Home</div>
                     </div>
-                    <div className="flex space-x-2">
-                        <div>Icon</div>
+                    <div className="flex space-x-2 items-center">
+                        <div><FaUser /></div>
                         <div>About</div>
                     </div>
-                    <div className="flex space-x-2">
-                        <div>Icon</div>
+                    <div className="flex space-x-2 items-center">
+                        <div><FaAddressCard /></div>
                         <div>Resume</div>
                     </div>
-                    <div className="flex space-x-2">
-                        <div>Icon</div>
+                    <div className="flex space-x-2 items-center">
+                        <div><FaServicestack /></div>
                         <div>Services</div>
                     </div>
-                    <div className="flex space-x-2">
-                        <div>Icon</div>
+                    <div className="flex space-x-2 items-center">
+                        <div><FaVrCardboard /></div>
                         <div>Portfolio</div>
                     </div>
-                    <div className="flex space-x-2">
-                        <div>Icon</div>
+                    <div className="flex space-x-2 items-center">
+                        <div><FaSalesforce /></div>
                         <div>Pricing</div>
                     </div>
-                    <div className="flex space-x-2">
-                        <div>Icon</div>
+                    <div className="flex space-x-2 items-center">
+                        <div><FaCamera /></div>
                         <div>Blog</div>
                     </div>
-                    <div className="flex space-x-2">
-                        <div>Icon</div>
+                    <div className="flex space-x-2 items-center">
+                        <div><FaFileContract /></div>
                         <div>Contact</div>
                     </div>
                 </div>
-                <div className="flex">
-                    <div>Icon</div>
+                <div className="flex space-x-2 items-center">
+                    <div><FaDownload /></div>
                     <div>Download CV</div>
                 </div>
             </div>
